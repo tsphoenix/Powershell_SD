eea7e3893ae115cf7f9374ebd1820bf37336d246
tsphoenix <46634852+tsphoenix@users.noreply.github.com>
2019-10-21 01:19:32 +0200

    Array  ForEach-Object Stop

--- wipe_ivanti_appsensevirtual_prv/wipe_ivanti_appsensevirtual_prv.ps1
+++ wipe_ivanti_appsensevirtual_prv/wipe_ivanti_appsensevirtual_prv.ps1
@@ -1,27 +1,7 @@
 #IVANTI APPSENSEVIRTUAL CLEANER @tsphoenix
 
-Stop-Process -Name "iexplore" 
-Stop-Process -Name "iexplore" -Force
-Stop-Process -Name "winword" 
-Stop-Process -Name "winword" -Force
-Stop-Process -Name "excel" 
-Stop-Process -Name "excel" -Force
-Stop-Process -Name "powerpnt" 
-Stop-Process -Name "powerpnt" -Force
-Stop-Process -Name "msaccess" 
-Stop-Process -Name "msaccess" -Force
-Stop-Process -Name "outlook" 
-Stop-Process -Name "outlook" -Force
-Stop-Process -Name "visio" 
-Stop-Process -Name "visio" -Force
-Stop-Process -Name "winproj" 
-Stop-Process -Name "winproj" -Force
-Stop-Process -Name "lync" 
-Stop-Process -Name "lync" -Force
-Stop-Process -Name "mspub" 
-Stop-Process -Name "mspub" -Force
-Stop-Process -Name "onenote" 
-Stop-Process -Name "onenote" -Force
+$processes = "iexplore","winword","excel","powerpnt","msaccess","outlook","visio","winproj","lync","mspub","onenote"
+$processes | ForEach-Object { Get-Process -Name $_ | Stop-Process -Force}
 
 $path = "C:\appsensevirtual\"
 Get-ChildItem -Path $path -Recurse| Foreach-object {Remove-item -Recurse -path $_.FullName }
