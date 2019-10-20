aee6e504468deb99c617854d00261d0d95ec9755
tsphoenix <46634852+tsphoenix@users.noreply.github.com>
2019-10-20 23:40:34 +0200

    batch title added

--- wipe_ivanti_appsensevirtual_prv/wipe_ivanti_appsensevirtual_prv_launcher.bat
+++ wipe_ivanti_appsensevirtual_prv/wipe_ivanti_appsensevirtual_prv_launcher.bat
@@ -1,3 +1,5 @@
-@ECHO OFF
+title wipe_ivanti_appsensevirtual_prv
+
+ECHO OFF
 PowerShell.exe -NoProfile -Command "& {Start-Process PowerShell.exe -Credential $Creds -ArgumentList '-ExecutionPolicy Bypass -File wipe_ivanti_appsensevirtual_prv.ps1'}"
 PAUSE
