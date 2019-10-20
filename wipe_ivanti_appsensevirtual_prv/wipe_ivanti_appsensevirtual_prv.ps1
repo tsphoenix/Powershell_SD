742b4f195b535de545abf7a06a4f08436a075a86
tsphoenix <46634852+tsphoenix@users.noreply.github.com>
2019-10-21 01:22:51 +0200

    whitespaces

--- wipe_ivanti_appsensevirtual_prv/wipe_ivanti_appsensevirtual_prv.ps1
+++ wipe_ivanti_appsensevirtual_prv/wipe_ivanti_appsensevirtual_prv.ps1
@@ -1,7 +1,7 @@
 #IVANTI APPSENSEVIRTUAL CLEANER @tsphoenix
 
 $processes = "iexplore","winword","excel","powerpnt","msaccess","outlook","visio","winproj","lync","mspub","onenote"
-$processes | ForEach-Object { Get-Process -Name $_ | Stop-Process -Force}
+$processes | ForEach-Object { Get-Process -Name $_ | Stop-Process -Force }
 
 $path = "C:\appsensevirtual\"
-Get-ChildItem -Path $path -Recurse| Foreach-object {Remove-item -Recurse -path $_.FullName }
+Get-ChildItem -Path $path -Recurse| Foreach-object { Remove-item -Recurse -path $_.FullName }
