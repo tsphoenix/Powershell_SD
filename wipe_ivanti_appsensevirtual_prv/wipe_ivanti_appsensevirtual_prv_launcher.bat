6485734c51ecf82c108a451994891adb905c652b
tsphoenix <46634852+tsphoenix@users.noreply.github.com>
2019-10-21 00:15:29 +0200

    ECHO feedback, title fix

--- wipe_ivanti_appsensevirtual_prv/wipe_ivanti_appsensevirtual_prv_launcher.bat
+++ wipe_ivanti_appsensevirtual_prv/wipe_ivanti_appsensevirtual_prv_launcher.bat
@@ -1,5 +1,10 @@
-title wipe_ivanti_appsensevirtual_prv
-
-ECHO OFF
+@ECHO OFF
+title IVANTI APPSENSEVIRTUAL CLEANER
+ECHO.
+ECHO PLEASE PROVIDE PROPER ELEVATED CREDENTIALS
+ECHO.
 PowerShell.exe -NoProfile -Command "& {Start-Process PowerShell.exe -Credential $Creds -ArgumentList '-ExecutionPolicy Bypass -File .\wipe_ivanti_appsensevirtual_prv.ps1'}"
+ECHO.
+ECHO Inanti C:\appsensevirtual\ CLEARED
+ECHO.
 PAUSE
