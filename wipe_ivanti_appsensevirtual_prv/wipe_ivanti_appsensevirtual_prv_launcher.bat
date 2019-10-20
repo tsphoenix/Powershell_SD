6bc9d873be8e29463ed128ad83749fb534a758aa
tsphoenix <46634852+tsphoenix@users.noreply.github.com>
2019-10-21 00:01:29 +0200

    fixed Relative path (same folder)

--- wipe_ivanti_appsensevirtual_prv/wipe_ivanti_appsensevirtual_prv_launcher.bat
+++ wipe_ivanti_appsensevirtual_prv/wipe_ivanti_appsensevirtual_prv_launcher.bat
@@ -1,5 +1,5 @@
 title wipe_ivanti_appsensevirtual_prv
 
 ECHO OFF
-PowerShell.exe -NoProfile -Command "& {Start-Process PowerShell.exe -Credential $Creds -ArgumentList '-ExecutionPolicy Bypass -File wipe_ivanti_appsensevirtual_prv.ps1'}"
+PowerShell.exe -NoProfile -Command "& {Start-Process PowerShell.exe -Credential $Creds -ArgumentList '-ExecutionPolicy Bypass -File .\wipe_ivanti_appsensevirtual_prv.ps1'}"
 PAUSE
