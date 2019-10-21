335e17033cea96f4070c0afd5c04c40790cbb6d5
tsphoenix <46634852+tsphoenix@users.noreply.github.com>
2019-10-21 19:43:01 +0200

    fixed path whitespace typo

--- kill_all_user_apps/kill_all_user_apps_launcher.bat
+++ kill_all_user_apps/kill_all_user_apps_launcher.bat
@@ -1,6 +1,6 @@
 @ECHO OFF
 title ALL USER LEVEL APPS KILLER
-PowerShell.exe -NoProfile -Command "& {Start-Process PowerShell.exe -ArgumentList '-ExecutionPolicy Bypass -File .\ 	kill_all_user_apps.ps1'}"
+PowerShell.exe -NoProfile -Command "& {Start-Process PowerShell.exe -ArgumentList '-ExecutionPolicy Bypass -File .\kill_all_user_apps.ps1'}"
 ECHO.
 ECHO ALL USER LEVEL APPS KILLED
 ECHO.
