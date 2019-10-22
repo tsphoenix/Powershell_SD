9bcfca602c1c1957ea2742736b0b04212963da14
tsphoenix <46634852+tsphoenix@users.noreply.github.com>
2019-10-23 00:05:26 +0200

    peudocode...

--- Win_10_IE_Cleanup_User_Level/Win_10_IE_Cleanup_User_Level.ps1
+++ Win_10_IE_Cleanup_User_Level/Win_10_IE_Cleanup_User_Level.ps1
@@ -4,6 +4,8 @@ Stop-Process -Name "iexplore"
 Stop-Process -Name "iexplore" -Force
 
 #Pseudocode
-$Paths = PLACEHOLDER
+$Paths = "path1", "path2", "path3"
 
+ForEach($Path in $Paths) {
 Get-ChildItem -Path $Paths -Recurse | ForEach-Object { Remove-Item -Recurse -Path $_.FullName }
+{
