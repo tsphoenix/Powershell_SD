a646a896c7b4a225d6e8b54637e1355a13aa91c0
tsphoenix <46634852+tsphoenix@users.noreply.github.com>
2019-10-22 20:09:22 +0200

    Update and rename clean_user_TEMP_User_Level/clean_user_TEMP_User_Level_launcher.bat to user_TEMP_cleaner/user_TEMP_cleaner.bat

--- user_TEMP_cleaner/user_TEMP_cleaner.bat
+++ user_TEMP_cleaner/user_TEMP_cleaner.bat
@@ -1,5 +1,5 @@
 @ECHO OFF
-title TEMP CLEANER
+title USER TEMP CLEANER
 PowerShell.exe -NoProfile -Command "& {Start-Process PowerShell.exe -ArgumentList '-ExecutionPolicy Bypass -File .\clean_user_TEMP_User_Level.ps1'}"
 ECHO.
 ECHO USER TEMP FILES CLEANED
