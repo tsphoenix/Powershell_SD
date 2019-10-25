d8f9c7c38d25af8716179d9e170a3316044064f0
tsphoenix <46634852+tsphoenix@users.noreply.github.com>
2019-10-25 07:59:02 +0200

    YOU ARE STRONGLY ADVISED TO STOP IVANTI SERVICES FIRST

--- ivanti_appsensevirtual_cleaner_prv/ivanti_appsensevirtual_cleaner_prv_launcher.bat
+++ ivanti_appsensevirtual_cleaner_prv/ivanti_appsensevirtual_cleaner_prv_launcher.bat
@@ -1,6 +1,8 @@
 @ECHO OFF
 title IVANTI APPSENSEVIRTUAL CLEANER @tsphoenix
 ECHO.
+ECHO YOU ARE STRONGLY ADVISED TO STOP IVANTI SERVICES FIRST
+ECHO.
 ECHO PLEASE PROVIDE PROPER ELEVATED CREDENTIALS
 ECHO.
 PowerShell.exe -NoProfile -Command "& {Start-Process PowerShell.exe -Credential $Creds -ArgumentList '-ExecutionPolicy Bypass -File .\ivanti_appsensevirtual_cleaner_prv.ps1'}"
