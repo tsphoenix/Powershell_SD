628ffcb0e091ed971c0521d9fad1eccadbcc23f4
tsphoenix <46634852+tsphoenix@users.noreply.github.com>
2019-11-21 11:46:05 +0100

    Update Win_10_IE_Cleanup_User_Level_launcher.bat

--- Win_10_IE_Cleanup_User_Level/Win_10_IE_Cleanup_User_Level_launcher.bat
+++ Win_10_IE_Cleanup_User_Level/Win_10_IE_Cleanup_User_Level_launcher.bat
@@ -6,6 +6,6 @@ echo Win_10_IE_Cleanup_User_Level v2 @tsphoenix
 PowerShell.exe -NoProfile -Command "& {Start-Process PowerShell.exe -ArgumentList '-ExecutionPolicy Bypass -File .\Win_10_IE_Cleanup_User_Level.ps1'}"
 
 echo.
-echo Closed IE, INetCookies cleaned, echo INetCache cleaned, TEMP cleaned
+echo Closed IE, INetCookies cleaned, echo INetCache cleaned
 echo.
 pause
