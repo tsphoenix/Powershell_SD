671e6d2f01f199eaca44615aa144d4c489f15ab7
tsphoenix <46634852+tsphoenix@users.noreply.github.com>
2019-12-19 19:58:42 +0100

    Update Win_10_IE_Cleanup_User_Level.ps1

--- Win_10_IE_Cleanup_User_Level/Win_10_IE_Cleanup_User_Level.ps1
+++ Win_10_IE_Cleanup_User_Level/Win_10_IE_Cleanup_User_Level.ps1
@@ -1,6 +1,5 @@
 #Win_10_IE_Cleanup_User_Level v2  @tsphoenix
 
-Stop-Process -Name "iexplore" 
 Stop-Process -Name "iexplore" -Force
 
 # 4 2 1 8 16 32 128 = 191, better to put it in the array first and then sum use the sum of an array of numbers config like style
